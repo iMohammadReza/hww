7e57bbfa6010a7935f5b21c36e427107454af391
MohammadReza <mr3000persia@yahoo.com>
2017-12-15 18:16:51 +0330

    added timer for white state. Now score +1 when white and -1 when not

--- app/2player.js
+++ app/2player.js
@@ -6,10 +6,13 @@ export default class App extends Component {
 		super(props);
 		this.state = {
       player1Text : "Hit To Start!",
-      player2Text : "Hit To Start!"
+      player2Text : "Hit To Start!",
+      gradient : ["#E96443", "#904E95"],
+      buttonColor : "#ffffff"
       };
   }
   countDown;
+  whiterTimer;
   stage = "INIT"; // INIT, COUNTER, READY, WHITE, BREAK,
   scorePlayer1 = 0;
   scorePlayer2 = 0;
@@ -18,7 +21,10 @@ export default class App extends Component {
   isReadyPlayer2 = false;
 
   player1Click = () => {
-    switch (this.stage) {
+    if(this.scorePlayer1>9 || this.scorePlayer1<0){
+      //finish game
+    }else{
+      switch (this.stage) {
       case "INIT":
           this.isReadyPlayer1 = true;
           this.setState({player1Text: "Ready!"})
@@ -34,39 +40,67 @@ export default class App extends Component {
       case "COUNTER":
           break;
       case "READY":
-          this.scorePlayer1++;
-          this.setState({player1Text: this.scorePlayer1})
+        this.scorePlayer1--;
+        this.setState({player1Text: this.scorePlayer1})
+        this.setWhiter()
+        break;
+      case "WHITE":
+        this.scorePlayer1++;
+        this.setState({player1Text: this.scorePlayer1})
+        this.setWhiter()
+        break;
+        default:
+          break;
+      }
+    }
+  }
+
+  player2Click = () => {
+    if(this.scorePlayer2>9 || this.scorePlayer2<0){
+      //finish game
+    }else{
+      switch (this.stage) {
+        case "INIT":
+          this.isReadyPlayer2 = true;
+          this.setState({player2Text: "Ready!"})
+          if (this.isReadyPlayer1) {
+            this.stage = "COUNTER";
+            this.setState({player1Text: this.counter})
+            this.setState({player2Text: this.counter})
+            countDown = setInterval( () => {
+              this.setCounterPassed();
+            },1000);
+          }
+          break;
+        case "COUNTER":
+          break;
+        case "READY":
+          this.scorePlayer2--;
+          this.setState({player2Text: this.scorePlayer2})
+          clearTimeout(whiterTimer)
+          this.setWhiter()
+          break;
+        case "WHITE":
+          this.scorePlayer2++;
+          this.setState({player2Text: this.scorePlayer2})
+          clearTimeout(whiterTimer)
+          this.setWhiter()
           break;
         default:
           break;
       }
-  }
-
-  player2Click = () => {
-    switch (this.stage) {
-      case "INIT":
-        this.isReadyPlayer2 = true;
-        this.setState({player2Text: "Ready!"})
-        if (this.isReadyPlayer1) {
-          this.stage = "COUNTER";
-          this.setState({player1Text: this.counter})
-          this.setState({player2Text: this.counter})
-          countDown = setInterval( () => {
-            this.setCounterPassed();
-         },1000);
-        }
-        break;
-      case "COUNTER":
-        break;
-      case "READY":
-        this.scorePlayer2++;
-        this.setState({player2Text: this.scorePlayer2})
-        break;
-      default:
-        break;
     }
+    
   }
 
+  setWhiter = () => {
+    this.stage = "READY"
+    this.setState({gradient: ["#E96443", "#904E95"], buttonColor: "#ffffff"})
+    whiterTimer = setTimeout( () => {
+      this.stage = "WHITE"
+      this.setState({gradient: ["#ffffff", "#ffffff"], buttonColor: "#904E95"})
+    },2000);
+  }
 
   setCounterPassed = () => {
     this.counter--;
@@ -75,6 +109,7 @@ export default class App extends Component {
       this.setState({player1Text: "Hit When White!"})
       this.setState({player2Text: "Hit When White!"})
       this.stage = "READY";
+      this.setWhiter()
     }
     else if(this.counter==0){
       this.setState({player1Text: "GO!"})
@@ -85,12 +120,35 @@ export default class App extends Component {
       this.setState({player2Text: this.counter})
     }
   }
-  
 
   render() {
+    const styles = StyleSheet.create({
+      container: {
+        flex: 1,
+      },
+      button: {
+        flex:1,
+        margin:20,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderColor: this.state.buttonColor,
+        borderWidth: 3
+      },
+      judgeText: {
+        color: "white",
+        textAlign: "center",
+        fontSize: 25
+      },
+      buttonText: {
+        color: this.state.buttonColor,
+        textAlign: "center",
+        fontSize: 45
+      }
+    });
+
     return (
       <LinearGradient
-      colors={["#E96443", "#904E95"]}
+      colors={this.state.gradient}
       style={styles.container}>
         <StatusBar
           backgroundColor="transparent"
@@ -110,28 +168,3 @@ export default class App extends Component {
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    //backgroundColor: "#263238"
-  },
-  button: {
-    flex:1,
-    margin:20,
-		alignItems: 'center',
-		justifyContent: 'center',
-    borderColor: "white",
-    borderWidth: 3
-  },
-  judgeText: {
-    color: "white",
-    textAlign: "center",
-    fontSize: 25
-  },
-  buttonText: {
-    color: "white",
-    textAlign: "center",
-    fontSize: 45
-  }
-});
